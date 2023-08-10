122e186f9bd21881871b1e4b773309c05dfb82fe
mulukimwangeka <mwangekaruth@gmail.com>
2023-08-10 23:02:23 +0300

    bootstrap added in productlist and product

--- src/Product.js
+++ src/Product.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 const Product = ({ id, name, description, price, imageUrl }) => {
   return (
-    <div className="product">
-     <img src={imageUrl} alt={name} />
-      <h2>{name}</h2>
-      <p>{description}</p>
-      <p>Price: ${price}</p>
-      <button>Buy</button>
+    <div className="card product-card">
+      <img src={imageUrl} alt={name} className="card-img-top product-image" />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{description}</p>
+        <p className="card-text">${price}</p>
+        <a href="#" className="btn btn-primary">Add to Cart</a>
+      </div>
     </div>
   );
 };
--- src/ProductList.js
+++ src/ProductList.js
@@ -4,18 +4,20 @@ import trees from './data';
 
 const ProductList = () => {
   return (
-    <div className="product-list row">
-      {trees.map(tree => (
-        <div key={tree.id} className="col-md-4 mb-4">
-          <Product
-            id={tree.id}
-            name={tree.name}
-            description={tree.description}
-            price={tree.price}
-            imageUrl={tree.imageUrl}
-          />
-        </div>
-      ))}
+    <div className="container">
+      <div className="row">
+        {trees.map(tree => (
+          <div key={tree.id} className="col-md-4 mb-4">
+            <Product
+              id={tree.id}
+              name={tree.name}
+              description={tree.description}
+              price={tree.price}
+              imageUrl={tree.imageUrl}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
