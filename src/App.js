f162b63ed939496bffd812660061cf031a286fb1
mulukimwangeka <mwangekaruth@gmail.com>
2023-08-11 00:17:07 +0300

    home page added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import ProductList from './ProductList';
+import HomePage from './Home'; // Import the HomePage component
 
 function App() {
   return (
@@ -9,14 +10,13 @@ function App() {
         <h1 className="text-center">Tree Shop</h1>
       </header>
       <main className="container mt-4">
+        <HomePage /> {/* Add the HomePage component */}
         <ProductList />
       </main>
       <footer className="bg-secondary py-3 text-light text-center">
         {/* Footer content */}
       </footer>
-      
     </div>
-    
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mt-4">
+      <h2>Welcome to Tree Shop!</h2>
+      <p>
+        Trees are vital to our environment. They provide us with oxygen, absorb carbon dioxide,
+        and offer habitat to countless species of animals. Trees also have cultural and aesthetic
+        significance. At Tree Shop, we celebrate the beauty and importance of trees.
+      </p>
+      <p>
+        Our collection features a variety of tree species, each with unique characteristics and
+        benefits. From shade-providing oak trees to fruit-bearing apple trees, you'll find a diverse
+        selection to choose from.
+      </p>
+      <p>
+        Explore our products, learn about different tree types, and bring the beauty of nature into
+        your space. Feel free to browse our Tree Shop and discover the wonders of the plant world!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
