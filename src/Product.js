62b1ae2ea1100a3d566e983023c5c1fd2162f53c
mulukimwangeka <mwangekaruth@gmail.com>
2023-08-10 23:12:07 +0300

    added new products in data.js

--- src/Product.js
+++ src/Product.js
@@ -8,7 +8,7 @@ const Product = ({ id, name, description, price, imageUrl }) => {
         <h5 className="card-title">{name}</h5>
         <p className="card-text">{description}</p>
         <p className="card-text">${price}</p>
-        <a href="#" className="btn btn-primary">Add to Cart</a>
+        <a href="#" className="btn btn-primary">Buy Now</a>
       </div>
     </div>
   );
